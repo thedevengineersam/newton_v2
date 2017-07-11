106974255fd5b2d2a65e84818ae7c4c659290192
root <root@node1.wang.com>
2017-07-12 07:58:54 +0800

    Create start_service.sh

---
+++ test/start_service.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SERVICE=$1
+echo "Check $SERVICE status..."
+systemctl status  $SERVICE 1>/dev/null 2>&1
+flag=$?
+if [[ $flag = 0 ]] ;then
+	echo "$SERVICE is running."
+elif [[ $flag = 3 ]] ;then
+	echo "$SERVICE is starting from dead"
+	systemctl start $SERVICE
+	systemctl enable $SERVICE
+	echo "Done."
+fi
